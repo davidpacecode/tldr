7dfdef4e07db403a543d548d9e2ee696b13e7216
davidpacecode <davidpace@gmail.com>
2025-11-13 21:09:19 -0600

    initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,13 @@
+import "$styles/index.css"
+import "$styles/syntax-highlighting.css"
+
+// Example Web Awesome components. Mix 'n' match however you like!
+import "@awesome.me/webawesome/dist/components/button/button.js"
+import "@awesome.me/webawesome/dist/components/icon/icon.js"
+import "@awesome.me/webawesome/dist/components/spinner/spinner.js"
+import "/webawesome/dist/components/page/page.js";
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
---
+++ src/index.md
@@ -0,0 +1,12 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+
+layout: default
+---
+
+## too long; didn't read
+
+This is where the tl;drs will go....
+
+
+
