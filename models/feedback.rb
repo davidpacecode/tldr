ede980098bc3263c6a7adb36eedff917613a5622
davidpacecode <davidpace@gmail.com>
2025-11-17 22:35:58 -0600

    i think i finally have the basics of upvoting and downvoting working

--- models/feedback.rb
+++ models/feedback.rb
@@ -1,15 +1,14 @@
 class Feedback < Sequel::Model(:feedback)
   set_primary_key [:filename, :ip_address]
-  plugin :update_or_create
 end
 
 # Table: feedback
-# -----------------------------------
+# -------------------------------------
 # Primary Key: (filename, ip_address)
 # Columns:
 #  filename   | varchar(255) |
 #  ip_address | varchar(255) |
-#  vote_type  | varchar(255) |
+#  vote_type  | varchar(255) | NOT NULL
 #  created_at | timestamp    |
 #  updated_at | timestamp    |
-# -----------------------------------
+# -------------------------------------
